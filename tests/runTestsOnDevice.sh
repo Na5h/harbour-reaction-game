2ac60700bb032a665080c541de882dcab2f752eb
Nash <n45h.4pp5@gmail.com>
2014-11-09 15:21:21 +0200

    Removed some unecessary files

--- tests/runTestsOnDevice.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script for running tests. That's for specifying just one argument in QtCreator's configuration
-/usr/bin/tst-harbour-reaction-game -input /usr/share/tst-harbour-reaction-game
-
-# When you'll get some QML components in the main app, you'll need to import them to the test run
-# /usr/bin/tst-harbour-reaction-game -input /usr/share/tst-harbour-reaction-game -import /usr/share/harbour-reaction-game/qml/components
